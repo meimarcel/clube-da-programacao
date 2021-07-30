48f50c5546a5623878f1fb58e33d6bcf4eb246d4
Mei Marcel <mei.marcel05@gmail.com>
2021-07-29 21:08:47 -0300

    Correcao de layout

--- resources/views/layouts/app-guest.blade.php
+++ resources/views/layouts/app-guest.blade.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Clube da Programacao</title>
+	<title>{{ config('app.name', 'Laravel') }}</title>
     <script src="https://kit.fontawesome.com/c26b9223c8.js" crossorigin="anonymous"></script>
     <!-- Custom fonts for this template -->
 	<link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
