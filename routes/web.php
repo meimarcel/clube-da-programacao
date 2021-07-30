105701a85a356551bea814e24d6984727df26d87
Mei Marcel <mei.marcel05@gmail.com>
2021-07-29 22:40:38 -0300

    Forçar requisiçoes https

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,8 @@
 
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
